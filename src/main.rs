c1170dd883888fbe012424786d9ede189ebbc78c
Alden Davidson <adavidson+github@protonmail.ch>
2022-05-02 14:10:49 -0700

    retrieve files using their ID

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 extern crate rocket;
 mod paste_id;
 
+use std::fs::File;
+use std::io::prelude::Read;
+
 use paste_id::PasteId;
 use rocket::response::Debug;
 use rocket::data::ToByteUnit;
@@ -28,16 +31,25 @@ fn index() -> &'static str {
 
 #[post("/", data = "<paste>")]
 async fn upload(paste: Data<'_>) -> Result<String, Debug<std::io::Error>> {
-    let paste_id = PasteId::new(ID_SIZE);
-    let filepath = format!("upload/{id}", id = paste_id);
-    let url = format!("{host}/{id}\n", host = HOST, id = paste_id);
+    let id = PasteId::new(ID_SIZE);
+    let filepath = format!("upload/{id}", id = id);
+    let url = format!("{host}/{id}\n", host = HOST, id = id);
     
     paste.open(128.kibibytes()).into_file(filepath).await?;
 
     Ok(url)
 }
 
+#[get("/<id>")]
+async fn retrieve(id: &str) -> Result<String, Debug<std::io::Error>> {
+    let filepath = format!("upload/{id}", id = id);
+    let mut file = File::open(&filepath)?;
+    let mut string = String::new();
+    file.read_to_string(&mut string)?;
+    Ok(string)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, upload])
+    rocket::build().mount("/", routes![index, upload, retrieve])
 }
