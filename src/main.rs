22ae5c3e5e9c05c7db7e9dea285ae7fb6a95f439
Alden Davidson <adavidson+github@protonmail.ch>
2022-05-20 17:54:51 -0700

    Delete files per ID

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
 #[macro_use]
 extern crate rocket;
-mod paste_id;
 mod paste_form;
+mod paste_id;
 
-use std::io::Write;
-use std::fs::File;
-use rocket::fs::{FileServer, relative};
-use rocket::response::Debug;
 use rocket::data::ToByteUnit;
-use rocket::Data;
 use rocket::form::{Form, Strict};
+use rocket::fs::{relative, FileServer};
+use rocket::response::Debug;
+use rocket::Data;
+use std::fs::{remove_file, File};
+use std::io::Write;
 
-use paste_id::PasteId;
 use paste_form::PasteForm;
+use paste_id::PasteId;
 
 const ID_SIZE: usize = 3;
 const HOST: &str = "http://localhost:8000";
+const PASTE_LOCATION: &str = "upload";
+
+#[delete("/<id>")]
+async fn delete(id: PasteId<'_>) -> Result<(), Debug<std::io::Error>> {
+    let filepath = format!("{folder}/{id}", folder = PASTE_LOCATION, id = id);
+    match remove_file(filepath) {
+        Ok(_) => Ok(()),
+        Err(error) => Err(Debug(error)),
+    }
+}
 
 #[post("/", data = "<paste>")]
 async fn paste(paste: Form<Strict<PasteForm<'_>>>) -> Result<String, Debug<std::io::Error>> {
     let id = PasteId::new(ID_SIZE);
-    let filepath = format!("upload/{id}", id = id);
+    let filepath = format!("{folder}/{id}", folder = PASTE_LOCATION, id = id);
     let url = format!("{host}/{id}\n", host = HOST, id = id);
 
     let mut file = File::create(filepath)?;
@@ -34,9 +44,9 @@ async fn paste(paste: Form<Strict<PasteForm<'_>>>) -> Result<String, Debug<std::
 #[post("/", data = "<paste>", rank = 2)]
 async fn upload(paste: Data<'_>) -> Result<String, Debug<std::io::Error>> {
     let id = PasteId::new(ID_SIZE);
-    let filepath = format!("upload/{id}", id = id);
+    let filepath = format!("{folder}/{id}", folder = PASTE_LOCATION, id = id);
     let url = format!("{host}/{id}\n", host = HOST, id = id);
-    
+
     paste.open(128.kibibytes()).into_file(filepath).await?;
 
     Ok(url)
@@ -44,11 +54,13 @@ async fn upload(paste: Data<'_>) -> Result<String, Debug<std::io::Error>> {
 
 #[get("/<id>")]
 async fn retrieve(id: PasteId<'_>) -> Option<File> {
-    let filepath = format!("upload/{id}", id = id);
+    let filepath = format!("{folder}/{id}", folder = PASTE_LOCATION, id = id);
     File::open(&filepath).ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![upload, retrieve, paste]).mount("/", FileServer::from(relative!("frontend")))
+    rocket::build()
+        .mount("/", routes![upload, retrieve, paste, delete])
+        .mount("/", FileServer::from(relative!("html")))
 }
